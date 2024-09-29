2d8a3ac0f01abd087fb6a73f2c0c00d6bed5f35c
Sonikm <sonikm443@gmail.com>
2024-09-29 20:16:18 +0530

    maths

--- math/gcd_and_lcm.js
+++ math/gcd_and_lcm.js
@@ -1,25 +1,43 @@
-function findGCDAndLCM(n1, n2) {
-  let min = Math.min(n1, n2);
-  let max = Math.max(n1, n2);
-  let lcm, gcd;
+// function findGCDAndLCM(n1, n2) {
+//   let min = Math.min(n1, n2);
+//   let max = Math.max(n1, n2);
+//   let lcm, gcd;
 
-  //* GCD
-  for (let i = min; i > 0; i--) {
-    if (n1 % i === 0 && n2 % i === 0) {
-      gcd = i;
-      break;
+//   //* GCD
+//   for (let i = min; i > 0; i--) {
+//     if (n1 % i === 0 && n2 % i === 0) {
+//       gcd = i;
+//       break;
+//     }
+//   }
+
+//   //* LCM
+//   for (let i = max; i < n1 * n2; i++) {
+//     if (i % n1 === 0 && i % n2 === 0) {
+//       lcm = i;
+//       break;
+//     }
+//   }
+
+//   return [lcm, gcd];
+// }
+
+//* Using Euclidean algorithm
+function findGCDAndLCM(a, b) {
+  function gcd(a, b) {
+    while (a > 0 && b > 0) {
+      if (a > b) a = a % b;
+      else b = b % a;
     }
+
+    return a === 0 ? b : a;
   }
 
-  //* LCM
-  for (let i = max; i < n1 * n2; i++) {
-    if (i % n1 === 0 && i % n2 === 0) {
-      lcm = i;
-      break;
-    }
-  }
+  let hcf = gcd(a, b);
+  let lcm = (a * b) / hcf;
 
-  return [lcm, gcd];
+  console.log("GCD: ", hcf);
+  console.log("LCM: ", lcm);
 }
 
-console.log(findGCDAndLCM(12, 9));
+findGCDAndLCM(1785, 546)
